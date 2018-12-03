ecd3b3677690e3b877220b7b35525642900a22c9
govindvuppu <vuppugovind3@gmail.com>
2018-12-02 18:23:12 -0800

    5 commit

--- myscript2.js
+++ myscript2.js
@@ -94,27 +94,3 @@ function f() {
   console.log(5); // not reachable
 }
 f(); //console 0, 1, 3 ;return false
-
-//promises
-function imgLoad(url) {
-  return new Promise(function(resolve, reject) {
-    var request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.responseType = "blob";
-    request.onload = function() {
-      if (request.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(
-          Error(
-            "Image didn't load successfully; error code:" + request.statusText
-          )
-        );
-      }
-    };
-    request.onerror = function() {
-      reject(Error("There was a network error."));
-    };
-    request.send();
-  });
-}
